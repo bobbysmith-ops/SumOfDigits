287a9c4dc133102b77c1f3fd53141a0d5468be67
bobbysmith-ops <72006128+bobbysmith-ops@users.noreply.github.com>
2021-09-16 02:26:47 -0700

    Update Program.cs

--- SumOfDigits/Program.cs
+++ SumOfDigits/Program.cs
@@ -9,7 +9,7 @@ namespace SumOfDigits
             Console.Write("Input  a number(integer): ");
             int n = Convert.ToInt32(Console.ReadLine());
             int sum = 0;
-            while (n != 0)
+            while (n != 0)///////
             {
                 sum += n % 10;
                 n /= 10;
